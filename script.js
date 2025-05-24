cf1c12bb2a68e4cbb3f15e01c177aaceb194165e
sujipinku07 <sujipinku07@gmail.com>
2025-05-24 21:53:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelector('.weather-button').addEventListener('click', function() {
+document.querySelector('#getWeatherBtn').addEventListener('click', function() {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=d2b7bc546a476ee61d5675dfb844c8c0')
         .then(response => {
             if (!response.ok) {
@@ -10,7 +10,7 @@ document.querySelector('.weather-button').addEventListener('click', function() {
             // Extract weather information from the data
             const weatherDescription = data.weather[0].description;
             // Update the weatherData div
-            document.querySelector('.weatherData').innerHTML = `Current Weather in London: ${weatherDescription}`;
+            document.querySelector('#weatherData').innerHTML = `Current Weather in London: ${weatherDescription}`;
         })
         .catch(error => {
             console.error('Error fetching the weather data:', error);
